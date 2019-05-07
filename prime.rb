e4aea082fa301c281ecb3ccdf657a92a37157744
Jasmine Chen <jasmine1226@gmail.com>
2019-05-07 01:44:54 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(num)
+  count = 2
+  if num <= 0
+    return false
+  else  
+    until count == num do
+      if num % count == 0
+        return false
+      else
+        count += 1
+      end
+    end
+    if count == num
+      return true
+    end
+  end
+end
